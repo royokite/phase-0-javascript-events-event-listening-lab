a44f754a1508ef4d7ee7b4108f0b55fb683cdef6
RoyOkite <royokite@gmail.com>
2022-08-09 21:42:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-function addingEventListener() {
+// Event listening lab!
+function clickDO() {
+    alert('FINYA TENA!!!')
 }
+
+function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', clickDO);
+}
+
+addingEventListener();
+
+// function addingEventListener() {
+//     const input = document.getElementById('button');
+//     input.addEventListener('click', function() {
+//         alert('FINYA TENA!!');
+//     })
+// }
+
+// addingEventListener();
